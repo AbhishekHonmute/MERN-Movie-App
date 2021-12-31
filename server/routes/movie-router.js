550cf0a94c7484ac7fc1caf28ce7470565a00369
AbhishekHonmute <honmuteabhishek@gmail.com>
2021-12-31 15:45:44 +0530

    List movies page completed

--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/movie", MovieCtrl.createMovie);
 router.put("/movie/:id", MovieCtrl.updateMovie);
 router.delete("/movie/:id", MovieCtrl.deleteMovie);
-router.get("/movie", MovieCtrl.getMovies);
+router.get("/movies", MovieCtrl.getMovies);
 router.get("/movie/:id", MovieCtrl.getMovieById);
 
 module.exports = router;
